5b9aa1c0d6b9a4f4f25cdc7af4858f5df7680559
Jagdeep Singh <59809683+jagdeepsgill888@users.noreply.github.com>
2025-02-28 08:09:06 +0000

    Update Q1 and finish lab

--- metre2time.js
+++ metre2time.js
@@ -1,8 +1,8 @@
 //Given the  width of a lawn in metres, length of a lawn in metres and the square metres cut per minute calculate the minutes it would take to mow that lawn. Test at least 3 lawn sizes
 
 function convertMetre2time(Width, Length, mowRate){
-    let Area = Width * Length;
-    let mowMintues = Area / mowRate;
+    let lawnArea = Width * Length;
+    let mowMintues = lawnArea / mowRate;
     return mowMintues;
 }
 export { convertMetre2time }
--- spec/feet2Acres_spec.js
+++ spec/feet2Acres_spec.js
@@ -2,21 +2,24 @@ import { convertFeet2Acre } from '../squareFeet2Acres.js'
 
 describe("A suite", function() {
     it("What is 50000 sqaure feet in Ares", function() {
-      let Acre = convertFeet2Acre(50000);
+      let squareFeet = 50000;
+      let Acre = convertFeet2Acre(squareFeet);
       expect(Acre.toFixed(2)).toBe("1.15");
     });
   });
 
  describe("A suite", function() {
     it("What is 60000 sqaure feet in Ares", function() {
-      let Acre = convertFeet2Acre(60000);
+      let squareFeet = 60000;
+      let Acre = convertFeet2Acre(squareFeet);
       expect(Acre.toFixed(2)).toBe("1.38");
     });
   });
   
   describe("A suite", function() {
     it("What is 65000 sqaure feet in Ares", function() {
-      let Acre = convertFeet2Acre(65000);
+      let squareFeet = 65000;
+      let Acre = convertFeet2Acre(squareFeet);
       expect(Acre.toFixed(2)).toBe("1.49");
     });
 
